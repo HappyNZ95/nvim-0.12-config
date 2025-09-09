98264a5eb05e648959785eaa92b88be9afbaa7b5
Hayden <htewini@gmail.com>
2025-09-09 14:25:54 +1200

    clean up init.lua (moved lsp)

--- init.lua
+++ init.lua
@@ -2,23 +2,8 @@ if vim.fn.has("win32") == 1 then
 	require("config.powershell")
 end
 
--- LSP --lsp
-vim.pack.add({
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
-})
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-tool-installer").setup({
-	ensure_installed = {
-		"lua_ls",
-		"stylua",
-		"pyright",
-		"omnisharp", --c_sharp
-	},
-})
+-- LSP --
+require("plugins.lsp")
 
 -- Treesitter --
 require("plugins.treesitter")
@@ -27,13 +12,13 @@ require("plugins.treesitter")
 require("plugins.conform")
 require("plugins.blink-cmp")
 
--- Config
-require("config.options")
-require("config.keymaps")
-
 -- Appearance & Themes
 require("plugins.themes.kanagawa")
 require("config.theme")
+
+-- Config
+require("config.options")
+require("config.keymaps")
 
 require("plugins.mini-statusline")
 
