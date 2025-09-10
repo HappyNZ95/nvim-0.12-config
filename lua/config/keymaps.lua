b5a21ec63fd25806727679a8661c5746157f82d8
HappyNZ95 <htewini@gmail.com>
2025-09-10 20:56:14 +1200

    terminal scratchpad

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,16 +3,26 @@ vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory in Oil" })
 vim.keymap.set("n", "gl", function()
-    vim.diagnostic.open_float()
-end, {desc = "Open Diagnostics in Float" })
+	vim.diagnostic.open_float()
+end, { desc = "Open Diagnostics in Float" })
 
 vim.keymap.set("n", "<leader>cf", function()
-    require("conform").format({
-        lsp_format = "fallback",
-    })
+	require("conform").format({
+		lsp_format = "fallback",
+	})
 end, { desc = "Format current file" })
 
 -- Map <leader> fp to open projects
 vim.keymap.set("n", "<leader>fp", ":ProjectFzf<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>|", ":vsplit<CR>")
+
+--open scratchpad terminal--
+vim.keymap.set("n", "<leader><Tab>", function()
+	-- Open a horizontal terminal height 15
+	vim.cmd("15split | terminal")
+	--Enter insert mode automatically
+	vim.cmd("startinsert")
+end, { noremap = true, silent = true })
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
